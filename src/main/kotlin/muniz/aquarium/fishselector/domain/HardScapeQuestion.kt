0d3404b1aa8a8cb00b9af55506233f09e0873fc6
mmuniz75 <muniz_marcelo@hotmail.com>
2022-01-13 11:36:20 -0300

    Teste qestions no weight

--- src/main/kotlin/muniz/aquarium/fishselector/domain/HardScapeQuestion.kt
+++ src/main/kotlin/muniz/aquarium/fishselector/domain/HardScapeQuestion.kt
@@ -1,54 +1,68 @@
 package muniz.aquarium.fishselector.domain
 
-import java.util.*
-
-
 enum class HardScapeQuestion(val displayText : String,
                              val isYesOrNoQuestion : Boolean = false) {
 
-    SUBSTRACT_KNOLEDGEMENT("Você sabe quantos kilos de substrato tem no seu aquário ?", true) {
-        override fun getNext(knowSubstract: Any?)  = if (Objects.nonNull(knowSubstract) &&
-                                                          knowSubstract as Boolean) SUBSTRACT_WEIGHT else SUBSTRACT_FRONT_HEIGHT
+    SUBSTRACT_KNOLEDGEMENT(HardScapeQuestion.SUBSTRACT_KNOLEDGEMENT_TEXT, true) {
+        override fun getNext(knowSubstract: Boolean?)  = if (knowSubstract!=null &&
+                                                             knowSubstract) SUBSTRACT_WEIGHT else SUBSTRACT_FRONT_HEIGHT
     },
-    SUBSTRACT_WEIGHT("Quantos kilos de substrato tem no seu aquário ?") {
-        override fun getNext(substractWeight: Any?) =ROCK_EXISTENCE
+    SUBSTRACT_WEIGHT(HardScapeQuestion.SUBSTRACT_WEIGHT_TEXT) {
+        override fun getNext(substractWeight: Boolean?) =ROCK_EXISTENCE
     },
-    SUBSTRACT_FRONT_HEIGHT("Quantos centimetros de substratos você tem na parte da frente do aquário ?") {
-        override fun getNext(frontHeight: Any?) = SUBSTRACT_BACK_HEIGHT
+    SUBSTRACT_FRONT_HEIGHT(HardScapeQuestion.SUBSTRACT_FRONT_HEIGHT_TEXT) {
+        override fun getNext(frontHeight: Boolean?) = SUBSTRACT_BACK_HEIGHT
     },
-    SUBSTRACT_BACK_HEIGHT("Quantos centimetros de substratos você tem na parte da trás do aquário ?") {
-        override fun getNext(backHeight: Any?) = ROCK_EXISTENCE
-    },
-
-    ROCK_EXISTENCE("Você possue rochas grandes no seu aquário ?", true) {
-        override fun getNext(hasRocks: Any?) = if (Objects.nonNull(hasRocks) &&
-                                                   hasRocks as Boolean) ROCK_KNOLEDGEMENT else WOOD_EXISTENCE
-    },
-    ROCK_KNOLEDGEMENT("Você sabe quantos kilos de rochas tem no seu aquário ?", true) {
-        override fun getNext(knowRocks: Any?) = if (Objects.nonNull(knowRocks) &&
-                                                    knowRocks as Boolean) ROCK_WEIGHT else ROCK_NUMBER
-    },
-    ROCK_WEIGHT("Quantas kilos de rochas você tem no seu aquário ?") {
-        override fun getNext(rocksWeight: Any?) = WOOD_EXISTENCE
-    },
-    ROCK_NUMBER("Quantas rochas você tem no seu aquário ?") {
-        override fun getNext(knowCount: Any?) = WOOD_EXISTENCE
+    SUBSTRACT_BACK_HEIGHT(HardScapeQuestion.SUBSTRACT_BACK_HEIGHT_TEXT) {
+        override fun getNext(backHeight: Boolean?) = ROCK_EXISTENCE
     },
 
-    WOOD_EXISTENCE("Você possue troncos grandes no seu aquário ?", true) {
-        override fun getNext(hasWood: Any?) = if (Objects.nonNull(hasWood) && hasWood as Boolean) WOOD_KNOLEDGEMENT else null
+    ROCK_EXISTENCE(HardScapeQuestion.ROCK_EXISTENCE_TEXT, true) {
+        override fun getNext(hasRocks: Boolean?) = if (hasRocks !=null &&
+                                                       hasRocks) ROCK_KNOLEDGEMENT else WOOD_EXISTENCE
     },
-    WOOD_KNOLEDGEMENT("Você sabe quantos kilos de troncos tem no seu aquário ?", true) {
-        override fun getNext(knowWood: Any?) = if (Objects.nonNull(knowWood) && knowWood as Boolean) WOOD_WEIGHT else WOOD_NUMBER
+    ROCK_KNOLEDGEMENT(HardScapeQuestion.ROCK_KNOLEDGEMENT_TEXT, true) {
+        override fun getNext(knowRocks: Boolean?) = if (knowRocks!=null &&
+                                                        knowRocks) ROCK_WEIGHT else ROCK_NUMBER
     },
-    WOOD_WEIGHT("Quantos kilos de troncos tem no seu aquário ?") {
-        override fun getNext(knowWood: Any?) = null
+    ROCK_WEIGHT(HardScapeQuestion.ROCK_WEIGHT_TEXT) {
+        override fun getNext(rocksWeight: Boolean?) = WOOD_EXISTENCE
     },
-    WOOD_NUMBER("Quantos troncos você tem no seu aquário ?") {
-        override fun getNext(woodCount: Any?) = null
+    ROCK_NUMBER(HardScapeQuestion.ROCK_NUMBER_TEXT) {
+        override fun getNext(knowCount: Boolean?) = WOOD_EXISTENCE
+    },
+
+    WOOD_EXISTENCE(HardScapeQuestion.WOOD_EXISTENCE_TEXT, true) {
+        override fun getNext(hasWood: Boolean?) = if (hasWood!=null &&
+                                                      hasWood) WOOD_KNOLEDGEMENT else null
+    },
+    WOOD_KNOLEDGEMENT(HardScapeQuestion.WOOD_KNOLEDGEMENT_TEXT, true) {
+        override fun getNext(knowWood: Boolean?) = if (knowWood!=null &&
+                                                       knowWood) WOOD_WEIGHT else WOOD_NUMBER
+    },
+    WOOD_WEIGHT(HardScapeQuestion.WOOD_WEIGHT_TEXT) {
+        override fun getNext(knowWood: Boolean?) = null
+    },
+    WOOD_NUMBER(HardScapeQuestion.WOOD_NUMBER_TEXT) {
+        override fun getNext(woodCount: Boolean?) = null
     }
     ;
 
-    abstract fun getNext(value: Any? = null) : HardScapeQuestion?
+    companion object{
+        const val SUBSTRACT_KNOLEDGEMENT_TEXT = "Você sabe quantos kilos de substrato tem no seu aquário ?"
+        const val SUBSTRACT_WEIGHT_TEXT = "Quantos kilos de substrato tem no seu aquário ?"
+        const val SUBSTRACT_FRONT_HEIGHT_TEXT = "Quantos centimetros de substratos você tem na parte da frente do aquário ?"
+        const val SUBSTRACT_BACK_HEIGHT_TEXT= "Quantos centimetros de substratos você tem na parte da trás do aquário ?"
+        const val ROCK_EXISTENCE_TEXT = "Você possue rochas grandes no seu aquário ?"
+        const val ROCK_KNOLEDGEMENT_TEXT = "Você sabe quantos kilos de rochas tem no seu aquário ?"
+        const val ROCK_WEIGHT_TEXT = "Quantas kilos de rochas você tem no seu aquário ?"
+        const val ROCK_NUMBER_TEXT = "Quantas rochas você tem no seu aquário ?"
+        const val WOOD_EXISTENCE_TEXT = "Você possue troncos grandes no seu aquário ?"
+        const val WOOD_KNOLEDGEMENT_TEXT = "Você sabe quantos kilos de troncos tem no seu aquário ?"
+        const val WOOD_WEIGHT_TEXT = "Quantos = kilos de troncos tem no seu aquário ?"
+        const val WOOD_NUMBER_TEXT = "Quantos troncos você tem no seu aquário ?"
+    }
+
+    abstract fun getNext(yes: Boolean? = null) : HardScapeQuestion?
 
 }
---
+++ src/test/kotlin/muniz/aquarium/fishselector/domain/HardScapeQuestionTest.kt
@@ -0,0 +1,55 @@
+package muniz.aquarium.fishselector.domain
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Test
+
+class HardScapeQuestionTest {
+
+
+    @Test
+    fun notKnowNumbers(){
+        var curentQuestion : HardScapeQuestion? = HardScapeQuestion.SUBSTRACT_KNOLEDGEMENT
+        println(curentQuestion?.displayText)
+        assertQuestion(HardScapeQuestion.SUBSTRACT_KNOLEDGEMENT_TEXT, curentQuestion)
+        curentQuestion = nextQuestion(curentQuestion,false)
+
+        assertQuestion(HardScapeQuestion.SUBSTRACT_FRONT_HEIGHT_TEXT, curentQuestion)
+        curentQuestion = nextQuestion(curentQuestion)
+
+        assertQuestion(HardScapeQuestion.SUBSTRACT_BACK_HEIGHT_TEXT, curentQuestion)
+        curentQuestion = nextQuestion(curentQuestion)
+
+        assertQuestion(HardScapeQuestion.ROCK_EXISTENCE_TEXT, curentQuestion)
+        curentQuestion = nextQuestion(curentQuestion,true)
+
+        assertQuestion(HardScapeQuestion.ROCK_KNOLEDGEMENT_TEXT, curentQuestion)
+        curentQuestion = nextQuestion(curentQuestion, false)
+
+        assertQuestion(HardScapeQuestion.ROCK_NUMBER_TEXT, curentQuestion)
+        curentQuestion = nextQuestion(curentQuestion)
+
+        assertQuestion(HardScapeQuestion.WOOD_EXISTENCE_TEXT, curentQuestion)
+        curentQuestion = nextQuestion(curentQuestion,true)
+
+        assertQuestion(HardScapeQuestion.WOOD_KNOLEDGEMENT_TEXT, curentQuestion)
+        curentQuestion = nextQuestion(curentQuestion,false)
+
+        assertQuestion(HardScapeQuestion.WOOD_NUMBER_TEXT, curentQuestion)
+        curentQuestion = nextQuestion(curentQuestion)
+
+        assertNull(curentQuestion);
+
+    }
+
+    private fun assertQuestion(questionText: String, curentQuestion : HardScapeQuestion?){
+        assertEquals(questionText, curentQuestion?.displayText)
+    }
+
+    private fun nextQuestion(curentQuestion : HardScapeQuestion?, yes : Boolean? = null): HardScapeQuestion? {
+        val nextQuestion = curentQuestion?.getNext(yes)
+        println(nextQuestion?.displayText)
+        return nextQuestion
+    }
+}
