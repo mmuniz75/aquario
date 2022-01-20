7d5771d2738afd5e663191bfc9187c5d1601afb1
mmuniz75 <muniz_marcelo@hotmail.com>
2022-01-20 13:39:20 -0300

    Aquarium service test

--- src/test/kotlin/muniz/aquarium/fishselector/domain/AquariumServiceTest.kt
+++ src/test/kotlin/muniz/aquarium/fishselector/domain/AquariumServiceTest.kt
@@ -8,18 +8,28 @@ class AquariumServiceTest {
 
     @Test
     fun allTrueQuestions(){
-      val service = AquariumService()
-      var questions = service.addNextQuestion(null, null)
-
-      while(questions[questions.lastIndex] !=null){
-        val lastQuestion = questions[questions.lastIndex]
-        println(lastQuestion?.hardScapeQuestion?.displayText)
-        val answer = if (lastQuestion?.hardScapeQuestion?.isYesOrNoQuestion!!) true else 10
-        questions = service.addNextQuestion(HardScapeAnswer(lastQuestion.hardScapeQuestion,answer), questions)
-      }
-
-        val answers : List<HardScapeAnswer?> =  questions.toList()
-        val hardScape  =  HardScape(answers.filterNotNull())
+        val hardScape  =  HardScape(createHardScepe(true));
         Assertions.assertEquals(30, hardScape.height())
     }
+
+    @Test
+    fun allFalseQuestions(){
+        val hardScape  =  HardScape(createHardScepe(false),80,30);
+        Assertions.assertEquals(36, hardScape.height())
+    }
+
+    fun createHardScepe(yesForAll : Boolean): List<HardScapeAnswer> {
+        val service = AquariumService()
+        var questions = service.addNextQuestion(null, null)
+
+        while(questions[questions.lastIndex] !=null){
+            val lastQuestion = questions[questions.lastIndex]
+            println(lastQuestion?.hardScapeQuestion?.displayText)
+            val answer = if (lastQuestion?.hardScapeQuestion?.isYesOrNoQuestion!!) yesForAll else 10
+            questions = service.addNextQuestion(HardScapeAnswer(lastQuestion.hardScapeQuestion,answer), questions)
+        }
+
+        val answers : List<HardScapeAnswer?> =  questions.toList()
+        return answers.filterNotNull()
+     }
 }
