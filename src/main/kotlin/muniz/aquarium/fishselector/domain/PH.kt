753fe9af9ba0134291773fa1890c6634d1fc8820
mmuniz75 <muniz_marcelo@hotmail.com>
2022-01-20 14:49:35 -0300

    PH domain test

--- src/main/kotlin/muniz/aquarium/fishselector/domain/PH.kt
+++ src/main/kotlin/muniz/aquarium/fishselector/domain/PH.kt
@@ -2,8 +2,11 @@ package muniz.aquarium.fishselector.domain
 
 import java.math.BigDecimal
 
-class PH (name: String, min : BigDecimal, max : BigDecimal) {
+class PH (name: String, min : BigDecimal, max : BigDecimal = BigDecimal.ZERO) {
 
-    val displayRange : String = "${min.setScale(1)} - ${max.setScale(1)}"
+    val displayRange : String = if(max.equals(BigDecimal.ZERO))
+                                  min.toString()
+                                else
+                                 "${min.setScale(1)} - ${max.setScale(1)}"
 
 }
--- src/test/kotlin/muniz/aquarium/fishselector/domain/PHTest.kt
+++ src/test/kotlin/muniz/aquarium/fishselector/domain/PHTest.kt
@@ -12,4 +12,16 @@ class PHTest {
         val ph = PH("Acido",BigDecimal("6.2"), BigDecimal("6.8"))
           assertEquals("6.2 - 6.8", ph.displayRange)
     }
+
+    @Test
+    fun showNetralDisplay(){
+        val ph = PH("Neutro",BigDecimal("7.0"))
+        assertEquals("7.0", ph.displayRange)
+    }
+
+    @Test
+    fun showAlcaliDisplay(){
+        val ph = PH("Alcalino",BigDecimal("7.2"), BigDecimal("7.4"))
+        assertEquals("7.2 - 7.4", ph.displayRange)
+    }
 }
