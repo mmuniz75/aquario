a5027930fcbc04fc327fd918e5db8ae844d83580
mmuniz75 <muniz_marcelo@hotmail.com>
2022-02-04 13:57:38 -0300

    update db data

--- src/main/scripts/data_fish.sql
+++ src/main/scripts/data_fish.sql
@@ -1,148 +1,179 @@
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
 VALUES(1, 'Borboleta', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(1, 1);
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
 VALUES(2, 'Acará Bandeira', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(2, 1);
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
 VALUES(4, 'Barbo', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(4, 1);
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
 VALUES(5, 'Beijador', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(5, 2);
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
 VALUES(6, 'Betta', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(6, 2);
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
 VALUES(7, 'Botia', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(7, 2);
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
 VALUES(8, 'Cascudo', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(8, 1);
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(8, 2);
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
 VALUES(9, 'Peixe Gato', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(9, 1);
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
 VALUES(10, 'Ciclídeos Africanos Malawi', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(10, 3);
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
 VALUES(11, 'Ciclídeos Africanos Tanganyika', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(11, 3);
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
 VALUES(12, 'Ciclídeos Africanos Victoria', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(12, 4);
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
 VALUES(13, 'Ciclídeos Anões Sul Americanos', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(13, 1);
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(13, 'Ciclídeos América Central/Norte', 5, 6, 24, 28, 80, 30,'');
+VALUES(14, 'Ciclídeos América Central/Norte', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(14, 1);
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
 VALUES(15, 'Ciclídeos Maiores Sul Americanos', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(15, 1);
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
 VALUES(16, 'Corydoras', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(16, 2);
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(17, 'Cruzeiro do Sul', 5, 6, 24, 28, 80, 30,'')
+VALUES(17, 'Cruzeiro do Sul', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(17, 1);
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(18, 'Danio', 5, 6, 24, 28, 80, 30,'')
+VALUES(18, 'Danio', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(18, 2);
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(19, 'Acará Disco', 5, 6, 24, 28, 80, 30,'')
+VALUES(19, 'Acará Disco', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(19, 1);
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(20, 'Espada', 5, 6, 24, 28, 80, 30,'')
+VALUES(20, 'Espada', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(20, 3);
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(21, 'Kinguio', 5, 6, 24, 28, 80, 30,'')
+VALUES(21, 'Kinguio', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(21, 2);
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(22, 'Colisa', 5, 6, 24, 28, 80, 30,'')
+VALUES(22, 'Colisa', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(22, 1);
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(23, 'Lebiste', 5, 6, 24, 28, 80, 30,'')
+VALUES(23, 'Lebiste', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(23, 3);
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(24, 'Haplochromis', 5, 6, 24, 28, 80, 30,'')
+VALUES(24, 'Haplochromis', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(24, 4)
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(25, 'Camarão', 5, 6, 24, 28, 80, 30,'')
+VALUES(25, 'Camarão', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(25, 2)
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(26, 'Kilfishe', 5, 6, 24, 28, 80, 30,'')
+VALUES(26, 'Killifish', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(26, 2)
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(27, 'Faca', 5, 6, 24, 28, 80, 30,'')
+VALUES(27, 'Faca', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(27, 2)
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(29, 'Mocinha', 5, 6, 24, 28, 80, 30,'')
+VALUES(29, 'Mocinha', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(29, 1)
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(30, 'Molinésia', 5, 6, 24, 28, 80, 30,'')
+VALUES(30, 'Molinésia', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(30, 2)
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(31, 'Monos / Scats', 5, 6, 24, 28, 80, 30,'')
+VALUES(31, 'Monos / Scats', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(31, 4)
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(32, 'Moreia', 5, 6, 24, 28, 80, 30,'')
+VALUES(32, 'Moreia', 5, 6, 24, 28, 80, 30,'');
+INSERT INTO fish_ph (fish_id, ph_id) VALUES(32, 2)
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(33, 'Neon', 5, 6, 24, 28, 80, 30,'')
+VALUES(33, 'Neon', 5, 6, 24, 28, 80, 30,'');
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(34, 'Oscar', 5, 6, 24, 28, 80, 30,'')
+VALUES(34, 'Oscar', 5, 6, 24, 28, 80, 30,'');
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(35, 'Paulistinha', 5, 6, 24, 28, 80, 30,'')
+VALUES(35, 'Paulistinha', 5, 6, 24, 28, 80, 30,'');
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(36, 'Arco Íris', 5, 6, 24, 28, 80, 30,'')
+VALUES(36, 'Arco Íris', 5, 6, 24, 28, 80, 30,'');
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(37, 'Mato Grosso', 5, 6, 24, 28, 80, 30,'')
+VALUES(37, 'Mato Grosso', 5, 6, 24, 28, 80, 30,'');
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(39, 'Plati', 5, 6, 24, 28, 80, 30,'')
+VALUES(39, 'Plati', 5, 6, 24, 28, 80, 30,'');
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(40, 'Lagosta', 5, 6, 24, 28, 80, 30,'')
+VALUES(40, 'Lagosta', 5, 6, 24, 28, 80, 30,'');
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(41, 'Rasbora', 5, 6, 24, 28, 80, 30,'')
+VALUES(41, 'Rasbora', 5, 6, 24, 28, 80, 30,'');
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(42, 'Rosáceo', 5, 6, 24, 28, 80, 30,'')
+VALUES(42, 'Rosáceo', 5, 6, 24, 28, 80, 30,'');
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(43, 'Ramirezi', 5, 6, 24, 28, 80, 30,'')
+VALUES(43, 'Ramirezi', 5, 6, 24, 28, 80, 30,'');
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(44, 'Labeo', 5, 6, 24, 28, 80, 30,'')
+VALUES(44, 'Labeo', 5, 6, 24, 28, 80, 30,'');
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(45, 'Limpa Vidro', 5, 6, 24, 28, 80, 30,'')
+VALUES(45, 'Limpa Vidro', 5, 6, 24, 28, 80, 30,'');
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(46, 'Synodontis', 5, 6, 24, 28, 80, 30,'')
+VALUES(46, 'Synodontis', 5, 6, 24, 28, 80, 30,'');
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(47, 'Tetra', 5, 6, 24, 28, 80, 30,'')
+VALUES(47, 'Tetra', 5, 6, 24, 28, 80, 30,'');
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(48, 'Tricogaster Leeri', 5, 6, 24, 28, 80, 30,'')
+VALUES(48, 'Tricogaster Leeri', 5, 6, 24, 28, 80, 30,'');
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(49, 'Tricogaster', 5, 6, 24, 28, 80, 30,'')
+VALUES(49, 'Tricogaster', 5, 6, 24, 28, 80, 30,'');
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
-VALUES(50, 'Tanicti', 5, 6, 24, 28, 80, 30,'')
+VALUES(50, 'Tanicti', 5, 6, 24, 28, 80, 30,'');
 
 
 INSERT INTO fish_ph (fish_id, ph_id) VALUES(3, 1);
-INSERT INTO fish_ph (fish_id, ph_id) VALUES(3, 2);
+
 
 INSERT INTO fish_compatility (fish_id, compatible_fish_id) VALUES(1, 2);
 INSERT INTO fish_compatility (fish_id, compatible_fish_id) VALUES(1, 3);
--- src/main/scripts/ph.sql
+++ src/main/scripts/ph.sql
@@ -5,6 +5,8 @@ create table PH (
     maxPh numeric
     )
 ;
+INSERT INTO ph (id, name, minph, maxph) VALUES(0, 'Muito Acido', 4.0, 6.0);
 INSERT INTO ph (id, name, minph, maxph) VALUES(1, 'Acido', 6.2, 6.8);
 INSERT INTO ph (id, name, minph) VALUES (2, 'Neutro', 7.0, 7.0);
 INSERT INTO ph (id, name, minph, maxph) VALUES(3, 'Alcalino', 7.2, 7.4);
+INSERT INTO ph (id, name, minph, maxph) VALUES(4, "Muito Alcalino', 7.5, 8.0);
