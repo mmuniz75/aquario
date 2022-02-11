f07dc776bddd182e745ab4e9236841ed0a6a3455
mmuniz75 <muniz_marcelo@hotmail.com>
2022-02-11 10:14:08 -0300

    fish compatibility data

--- src/main/scripts/data_fish.sql
+++ src/main/scripts/data_fish.sql
@@ -339,6 +339,25 @@ VALUES(41, 'Rasbora', 3, 10, 24, 28, 60, 30,'http://www.aquarismopaulista.com/wp
 INSERT INTO fish_ph (fish_id, ph_id) VALUES(41, 4);
 INSERT INTO fish_dh (fish_id, dh_id) VALUES(41, 2);
 INSERT INTO fish_dh (fish_id, dh_id) VALUES(41, 3);
+INSERT INTO fish_compatility (fish_id, compatible_fish_id) VALUES(41, 1); -- Borboleta
+INSERT INTO fish_compatility (fish_id, compatible_fish_id) VALUES(41, 13); -- Apistograma
+INSERT INTO fish_compatility (fish_id, compatible_fish_id) VALUES(41, 17); -- Cruzeiro do sul
+INSERT INTO fish_compatility (fish_id, compatible_fish_id) VALUES(41, 26); -- Killifish
+INSERT INTO fish_compatility (fish_id, compatible_fish_id) VALUES(41, 29); -- Mocinha
+INSERT INTO fish_compatility (fish_id, compatible_fish_id) VALUES(41, 33); -- Neon
+INSERT INTO fish_compatility (fish_id, compatible_fish_id) VALUES(41, 37); -- Mato Grosso
+INSERT INTO fish_compatility (fish_id, compatible_fish_id) VALUES(41, 42); -- Rosaceo
+INSERT INTO fish_compatility (fish_id, compatible_fish_id) VALUES(41, 43); -- Ramirezi
+INSERT INTO fish_compatility (fish_id, compatible_fish_id) VALUES(41, 47); -- Tetra
+INSERT INTO fish_compatility (fish_id, compatible_fish_id) VALUES(41, 56); -- Tetra Negro
+INSERT INTO fish_compatility (fish_id, compatible_fish_id) VALUES(41, 52); -- Engraçãdinho
+INSERT INTO fish_compatility (fish_id, compatible_fish_id) VALUES(41, 7); -- Botia
+INSERT INTO fish_compatility (fish_id, compatible_fish_id) VALUES(41, 41); -- Rasbora
+INSERT INTO fish_compatility (fish_id, compatible_fish_id) VALUES(41, 22); -- Colisa
+INSERT INTO fish_compatility (fish_id, compatible_fish_id) VALUES(41, 4); -- Barbo
+INSERT INTO fish_compatility (fish_id, compatible_fish_id) VALUES(41, 2); -- Acará Bandiera
+INSERT INTO fish_compatility (fish_id, compatible_fish_id) VALUES(41, 8); -- Cascudo
+INSERT INTO fish_compatility (fish_id, compatible_fish_id) VALUES(41, 53); -- Rodostomo
 
 INSERT INTO fish (id, name, size, minnumber, mintemperature, maxtemperature, widthtank, lengthtank,imageurl)
 VALUES(7, 'Botia', 15, 6, 24, 28, 100, 40,'http://www.aquarismopaulista.com/wp-content/uploads/2020/01/Botia-almorhae2-678x381.jpg');
