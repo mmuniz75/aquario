3d2601bb94620174d209f3e1924995ca49303451
mmuniz75 <muniz_marcelo@hotmail.com>
2022-03-25 15:22:12 -0300

    add fish size to return

--- src/main/kotlin/muniz/aquarium/fishselector/dto/FishDTO.kt
+++ src/main/kotlin/muniz/aquarium/fishselector/dto/FishDTO.kt
@@ -8,11 +8,12 @@ class FishDTO (val id : Int,
                val temperatureRange : String,
                val phRange : String,
                val dhRange : String,
+               val size : Int,
                val imageUrl : String) {
 
     companion object {
         fun fromDomain(fish : Fish) : FishDTO{
-            return FishDTO(fish.id, fish.name, fish.minNumber, fish.temperatureRangeDisplay, fish.getPHRangeDisplay(),fish.getDHRangeDisplay(), fish.imageUrl)
+            return FishDTO(fish.id, fish.name, fish.minNumber, fish.temperatureRangeDisplay, fish.getPHRangeDisplay(),fish.getDHRangeDisplay(),fish.size, fish.imageUrl)
         }
     }
 }
